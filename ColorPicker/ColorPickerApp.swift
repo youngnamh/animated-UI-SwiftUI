8dbecc7a1cba6b993e6a8f3ef7a292932a2e7493
youngnamh <youngnamhlady@gmail.com>
2023-01-28 17:05:46 -0500

    color picker

---
+++ ColorPicker/ColorPickerApp.swift
@@ -0,0 +1,18 @@
+//
+//  ColorPickerApp.swift
+//  ColorPicker
+//
+//  Created by Bob  Jones on 2023-01-28.
+//
+
+import SwiftUI
+
+@main
+struct ColorPickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+
+    }
+}
---
+++ ColorPicker/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  ColorPicker
+//
+//  Created by Bob  Jones on 2023-01-28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Home()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
